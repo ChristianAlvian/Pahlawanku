c58c4b46a14a5d70c753c5344ac475beb6c290dd
ChristianAlvian <christianalviannn@gmail.com>
2022-11-16 17:04:38 +0700

    end 11-16-2022

--- app/src/main/java/com/chris/pahlawanku/adaptercard.java
+++ app/src/main/java/com/chris/pahlawanku/adaptercard.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
 
 public class adaptercard extends RecyclerView.Adapter<adaptercard.ClassViewHolder>{
@@ -30,6 +32,11 @@ public class adaptercard extends RecyclerView.Adapter<adaptercard.ClassViewHolde
         modelpahlawan pahlawan = datapahlawan.get(position);
         holder.tvNama.setText(pahlawan.getNama());
         holder.tvTentang.setText(pahlawan.getTentang());
+        Glide
+                .with(holder.itemView.getContext())
+                .load(pahlawan.getFoto())
+                .centerCrop()
+                .into(holder.ivFoto);
 
 
     }
